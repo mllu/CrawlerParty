a3ff828a6b4c324047a4f4c41c2fcb84cdf2922b
mllu <mllu@hac.tw>
2015-09-22 13:30:20 -0700

    move file into hw1

---
+++ hw1/read.py
@@ -0,0 +1,7 @@
+import nutchpy
+
+#node_path = "/Users/mllu/workspace/CrawlerParty/crawl/merged/20150922030212/parse_data/part-00000/data"
+node_path = "/Users/mllu/workspace/CrawlerParty/crawl/merged/20150922030212/content/part-00000/data"
+seq_reader = nutchpy.sequence_reader
+print(seq_reader.head(10,node_path))
+print(seq_reader.slice(10,20,node_path))

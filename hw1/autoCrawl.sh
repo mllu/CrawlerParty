cfcad5b4f48baac26de8f960d5c74974393b23e8
mllu <mllu@hac.tw>
2015-10-11 13:02:08 -0700

    modify autoCrawl.sh to output customized ditectory as 3rd arguments, add image domain discoverStuff

--- hw1/autoCrawl.sh
+++ hw1/autoCrawl.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "./autoCrawl.sh URL_FILE_NAME NUMBER_OF_RUN"
+if [ "$#" -ne 3 ]; then
+    echo "./autoCrawl.sh URL_FILE_NAME NUMBER_OF_RUN OUT_DIR"
     exit
 fi
-nutch inject crawl/crawldb $1
+nutch inject $3/crawldb $1
 
 for i in `seq 1 $2`;
 do
   echo "round $i"
   echo "=========== Generate Phase ==========="
-  nutch generate crawl/crawldb crawl/segments
-  seg=`ls -d crawl/segments/2* | tail -1`
+  nutch generate $3/crawldb $3/segments
+  seg=`ls -d $3/segments/2* | tail -1`
   echo $seg
   echo "=========== Fetch Phase ==========="
   nutch fetch $seg
   echo "=========== Parse Phase ==========="
   nutch parse $seg
   echo "=========== Update Phase ==========="
-  nutch updatedb crawl/crawldb $seg
+  nutch updatedb $3/crawldb $seg
 
   FILE="/tmp/outlinks"
   if [ -f "$FILE" ]
   then
     echo "=========== Inject new outlinks ==========="
-    nutch inject crawl/crawldb /tmp/outlinks
+    nutch inject $3/crawldb /tmp/outlinks
     mv /tmp/outlinks /tmp/outlinks$i
   fi
   printf "\n\n"

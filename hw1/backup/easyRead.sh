3e202803358de331ad89813632f22b8805d6a77c
Taichi <heyuehengtaichi@gmail.com>
2015-10-21 21:42:48 -0700

    organize the file

--- hw1/backup/easyRead.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-./runtime/local/bin/nutch mergesegs mergeSegments -dir crawl/segments 
-
-./runtime/local/bin/nutch readseg -dump mergeSegments/2* resultDump
-
-
--- hw1/backup/readDb.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash
-
-rm -rf dbDump
-./runtime/local/bin/nutch readdb crawl/crawldb/ -dump dbDump
-
